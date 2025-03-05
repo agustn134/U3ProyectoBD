c2c688e32eb70e8b2abfb997c4baf2eb7f407686
agustn134 <agustinparralopez1@gmail.com>
2025-03-04 20:26:24 -0600

    Cambios en component.html y el component home

--- FrontEnd/ProblematicaRegistroEmpleado/src/app/app.routes.ts
+++ FrontEnd/ProblematicaRegistroEmpleado/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { RegistroEmpleadoComponent } from './components/registro-empleado.component';
-import { ConsultaEmpleadoComponent } from './components/consulta-empleado/consulta-empleado.component';
 import { HomeComponent } from './components/home/home.component';
+import { ActividadesComponent } from './components/actividades/actividades.component';
+import { InicioEmpleadoComponent } from './components/inicio-empleado/inicio-empleado.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'registro-empleado', component: RegistroEmpleadoComponent }, // Verifica que esté bien escrito
-  { path: 'consulta-empleado', component: ConsultaEmpleadoComponent },
-  { path: '**', redirectTo: '' } // Redirección a la página principal si la ruta no existe
+  { path: 'actividades-empleado', component: ActividadesComponent }, // Verifica que esté bien escrito
+  { path: 'inicio-empleado', component: InicioEmpleadoComponent },
+  { path: '**', redirectTo: '/actividades-empleado' } // Redirección a la página principal si la ruta no existe
 ];
